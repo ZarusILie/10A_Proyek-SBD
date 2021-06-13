3e6ccb7614d0271990b0378447c3f9ecf8756a93
ZarusILie <76053246+ZarusILie@users.noreply.github.com>
2021-06-13 22:12:53 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,94 @@
+	
+	<!-- Start Contact info -->
+	<div class="contact-imfo-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 arrow-right">
+					<i class="fa fa-volume-control-phone"></i>
+					<div class="overflow-hidden">
+						<h4>Phone</h4>
+						<p class="lead">
+							+6285772868649
+						</p>
+					</div>
+				</div>
+				<div class="col-md-4 arrow-right">
+					<i class="fa fa-envelope"></i>
+					<div class="overflow-hidden">
+						<h4>Email</h4>
+						<p class="lead">
+							restoonline@gmail.com
+						</p>
+					</div>
+				</div>
+				
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End Contact info -->
+	
+	<!-- Start Footer -->
+	<footer class="footer-area bg-f">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3 col-md-6">
+					<h3>About</h3>
+					<p>we go online because we care</p>
+				</div>
+				<div class="col-lg-3 col-md-6">
+					<h3>UseFull Links</h3>
+					<div class="subscribe_form">
+						
+						<a href="index.php"><h3>Home</h3></a><br>
+						<a href="menu.php"><h3>Menu</h3></a><br>
+						<a href="gallery.php"><h3>Gallery</h3></a><br>
+						<a href="about.php"><h3>About</h3></a><br>
+						
+						
+					</div>
+					<ul class="list-inline f-social">
+						<li class="list-inline-item"><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+						<li class="list-inline-item"><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+						<li class="list-inline-item"><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+						<li class="list-inline-item"><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+						<li class="list-inline-item"><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+					</ul>
+				</div>
+				<div class="col-lg-3 col-md-6">
+					<h3>Contact information</h3>
+					<p class="lead">+6285772868649</p>
+		
+					<p><a href="#"> restoonline@gmail.com</a></p>
+				</div>
+				<div class="col-lg-3 col-md-6">
+					<h3>Opening hours</h3>
+					<p><span class="text-color">Monday: </span> 7:Am - 12PM</p>
+					<p><span class="text-color">Tue-Wed :</span> 7:Am - 12PM</p>
+					<p><span class="text-color">Thu-Fri :</span> 7:Am - 12PM</p>
+					<p><span class="text-color">Sat-Sun :</span> 7:Am - 12PM</p>
+				</div>
+			</div>
+		</div>
+		
+	
+		
+	</footer>
+	<!-- End Footer -->
+	
+	<a href="#" id="back-to-top" title="Back to top" style="display: none;"><i class="fa fa-paper-plane-o" aria-hidden="true"></i></a>
+
+	<!-- ALL JS FILES -->
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+    <!-- ALL PLUGINS -->
+	<script src="js/jquery.superslides.min.js"></script>
+	<script src="js/images-loded.min.js"></script>
+	<script src="js/isotope.min.js"></script>
+	<script src="js/baguetteBox.min.js"></script>
+	<script src="js/form-validator.min.js"></script>
+    <script src="js/contact-form-script.js"></script>
+    <script src="js/custom.js"></script>
+</body>
+</html>
