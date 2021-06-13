b7ace558512fcb78fd838edde4d5e1c64d96c06e
ZarusILie <76053246+ZarusILie@users.noreply.github.com>
2021-06-13 22:07:30 +0700

    Add files via upload

---
+++ admin/footer.php
@@ -0,0 +1,2 @@
+<br><br>
+<div class="footer"> kelompok sbd</div>
---
+++ admin/header.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" type="text/css" href="style2.css">
+<div class="h">Resto database </div>
+<div class="mbg">
+	<a href="after_login.php" class="menu">Orders</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<a href="food.php" class="menu">Menu</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<a href="gallery.php" class="menu">Gallery menu</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<a href="logout.php" class="menu" style="color: red;">Logout</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	
+</div>
+<br><br>
